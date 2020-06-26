ef1bf820ce8eb4fddb2da5548fd63ff6bc238e28
Nicolas Resnikow <nresnikow@gmail.com>
2020-06-26 10:19:57 +0200

    Torch

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,28 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_arg, _session, socket) do
+    socket = assign(socket, :brightness, 10)
+    {:ok, socket}
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, :brightness, 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    socket = update(socket, :brightness, &(&1 - 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    socket = update(socket, :brightness, &(&1 + 10))
+    {:noreply, socket}
+  end
+end

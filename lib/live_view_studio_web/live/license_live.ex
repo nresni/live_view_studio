673589d03e4f700d37f1450aa7e5ed296db0d9de
Nicolas Resnikow <nresnikow@gmail.com>
2020-06-26 13:14:15 +0200

    Handle countdown in license

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -5,7 +5,21 @@ defmodule LiveViewStudioWeb.LicenseLive do
   import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, seats: 3, amount: Licenses.calculate(3))
+    if connected?(socket) do
+      :timer.send_interval(1_000, self(), :tick)
+    end
+
+    expiration_time = Timex.shift(Timex.now(), hours: 1)
+
+    socket =
+      assign(
+        socket,
+        seats: 3,
+        amount: Licenses.calculate(3),
+        expiration_time: expiration_time,
+        time_remaining: time_remaining(expiration_time)
+      )
+
     {:ok, socket}
   end
 
@@ -15,6 +29,11 @@ defmodule LiveViewStudioWeb.LicenseLive do
     <div id="license">
       <div class="card">
         <div class="content">
+
+          <p class="m-4 font-semibold text-indigo-800">
+            <%= @time_remaining %> left to save 20%
+          </p>
+
           <div class="seats">
             <img src="images/license.svg">
             <span>
@@ -47,5 +66,18 @@ defmodule LiveViewStudioWeb.LicenseLive do
       )
 
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    expiration_time = socket.assigns.expiration_time
+    socket = assign(socket, time_remaining: time_remaining(expiration_time))
+    {:noreply, socket}
+  end
+
+  defp time_remaining(expiration_time) do
+    Timex.Interval.new(from: Timex.now(), until: expiration_time)
+    |> Timex.Interval.duration(:seconds)
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
   end
 end

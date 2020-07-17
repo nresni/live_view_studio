24ba5cd29b4a69987688eb4ec1c217f402a02ddb
Nicolas Resnikow <nresnikow@gmail.com>
2020-07-17 10:17:25 +0200

    Debounce

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -25,7 +25,7 @@ defmodule LiveViewStudioWeb.LicenseLive do
 
           <form phx-change="update">
             <input type="range" min="1" max="10"
-                  name="seats" value="<%= @seats %>" />
+                  name="seats" value="<%= @seats %>" phx-debounce="250" />
           </form>
 
           <div class="amount">
